9edc1b540e64d8963882cd83697e201cf5f06e28
Sobinjm <jmsobin@gmail.com>
2024-02-15 17:19:22 +0530

    php

---
+++ projects.php
@@ -0,0 +1,415 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <title>VYK Networks LLC | Your Telecom Partner</title>
+  <!-- Stylesheets -->
+  <link href="css/bootstrap.css" rel="stylesheet">
+  <link href="css/style.css" rel="stylesheet">
+  <link href="css/responsive.css" rel="stylesheet">
+
+  <!-- Color Switcher Mockup -->
+  <link href="css/color-switcher-design.css" rel="stylesheet">
+
+  <!-- Color Themes -->
+  <link id="theme-color-file" href="css/color-themes/default-color.css" rel="stylesheet">
+
+  <link
+    href="https://fonts.googleapis.com/css2?family=DM+Sans:opsz,wght@9..40,300;9..40,400;9..40,500;9..40,600;9..40,700;9..40,800;9..40,900;9..40,1000&amp;display=swap"
+    rel="stylesheet">
+
+  <link rel="shortcut icon" href="./images/Logo/vyklogo-01.png" type="image/x-icon">
+  <link rel="icon" href="./images/Logo/vyklogo-01.png" type="image/x-icon">
+
+  <!-- Responsive -->
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+
+</head>
+
+<body>
+<?php 
+
+
+$projects=
+[
+  array(
+  "logo"=>"./images/resource/Motrola.png",
+  "title"=>"Drive test services",
+  "period"=>"2021 - ONGOING",
+  "description"=>"<h5 class=\"gallery-block_one-heading fw-normal fs-6 text-justify split-in-left m-0 p-3\">".
+  "Country: Oman <br>".
+ " Account: Motorola<br>".
+  "Operator: ROP<br>".
+  "Structured cabling and installation of Workstations".
+"</h5>"
+  ),
+  array(
+    "logo"=>"./images/resource/Motrola.png",
+    "title"=>"Drive test services",
+    "period"=>"2021 - ONGOING",
+    "description"=>"<h5 class=\"gallery-block_one-heading fw-normal fs-6 text-justify split-in-left m-0 p-3\">".
+    "Country: Oman <br>".
+   " Account: Motorola<br>".
+    "Operator: ROP<br>".
+    "Structured cabling and installation of Workstations".
+  "</h5>"
+    ),
+    array(
+      "logo"=>"./images/resource/Motrola.png",
+      "title"=>"Drive test services",
+      "period"=>"2021 - ONGOING",
+      "description"=>"<h5 class=\"gallery-block_one-heading fw-normal fs-6 text-justify split-in-left m-0 p-3\">".
+      "Country: Oman <br>".
+     " Account: Motorola<br>".
+      "Operator: ROP<br>".
+      "Structured cabling and installation of Workstations".
+    "</h5>"
+      ),
+      array(
+        "logo"=>"./images/resource/Motrola.png",
+        "title"=>"Drive test services",
+        "period"=>"2021 - ONGOING",
+        "description"=>"<h5 class=\"gallery-block_one-heading fw-normal fs-6 text-justify split-in-left m-0 p-3\">".
+        "Country: Oman <br>".
+       " Account: Motorola<br>".
+        "Operator: ROP<br>".
+        "Structured cabling and installation of Workstations".
+      "</h5>"
+        ),
+array(
+  "logo"=>"./images/resource/Motrola.png",
+  "title"=>"",
+  "period"=>"",
+  "description"=>""
+)
+];
+
+$projects=$projects;
+
+?>
+  <div class="page-wrapper">
+
+    <!-- Preloader -->
+    <div class="preloader"></div>
+    <!-- End Preloader -->
+
+    <!-- Main Header / Style Three -->
+    <header class="main-header header-style-two">
+
+      <!-- Header Top -->
+      <div class="header-top">
+        <div class="px-0 auto-container ps-3 mw-100">
+          <div class="inner-container">
+            <div class="flex-wrap d-flex justify-content-between">
+              <ul class="header-list">
+                <li><span class="icon fas fa-clock fa-fw"></span>Mon - Sat: 8:00am - 6:00pm</li>
+                <li><span class="icon fas fa-envelope fa-fw"></span><a
+                    href="mailto:needhelp@company.com">info@vyknetworkllc.com</a></li>
+              </ul>
+              <div class="flex-wrap right-box d-flex align-items-center">
+
+                <!-- Social Box -->
+                <div class="header-social_box">
+                  <a href="https://facebook.com/"><i class="fa-brands fa-facebook-f"></i></a>
+                  <a href="https://twitter.com/"><i class="fa-brands fa-twitter"></i></a>
+                  <a href="https://youtube.com/"><i class="fa-brands fa-youtube"></i></a>
+                  <a href="https://instagram.com/"><i class="fa-brands fa-instagram"></i></a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <!-- Header Lower -->
+      <div class="header-lower">
+        <div class="auto-container">
+          <div class="inner-container d-flex align-items-center justify-content-between">
+
+            <!-- Logo Box -->
+            <div class="logo-box d-flex align-items-center">
+              <div class="logo"><a href="index.html"><img src="images/logo.png" alt="" title=""></a>
+              </div>
+            </div>
+
+            <div class="nav-outer d-flex align-items-center">
+
+              <!-- Main Menu -->
+              <nav class="main-menu show navbar-expand-md">
+                <div class="navbar-header">
+                  <button class="navbar-toggler" type="button" data-toggle="collapse"
+                    data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                    aria-label="Toggle navigation">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                  </button>
+                </div>
+
+                <div class="clearfix navbar-collapse collapse" id="navbarSupportedContent">
+                  <ul class="clearfix navigation">
+                    <li><a href="./index.html">Home</a></li>
+                    <li><a href="./about.html">About Us</a></li>
+                    <li><a href="./services.html">Services</a></li>
+                    <li><a href="./product.html">Product</a></li>
+                    <li><a href="./projects.html">Projects</a></li>
+                    <li><a href="./gallery.html">Gallery</a></li>
+                    <li><a href="./contact.html">Contact</a></li>
+                  </ul>
+                </div>
+              </nav>
+              <!-- Main Menu End-->
+            </div>
+
+            <!-- Outer Box -->
+            <div class="outer-box d-flex align-items-center">
+
+              <!-- Mobile Navigation Toggler -->
+              <div class="mobile-nav-toggler"><span class="icon"><img src="images/icons/menu.png" alt="" /></span></div>
+
+            </div>
+            <!-- End Outer Box -->
+
+          </div>
+
+        </div>
+      </div>
+      <!-- End Header Lower -->
+
+      <!-- Mobile Menu  -->
+      <div class="mobile-menu">
+        <div class="menu-backdrop"></div>
+        <div class="close-btn"><span class="icon far fa-times fa-fw"></span></div>
+        <nav class="menu-box">
+          <div class="nav-logo"><a href="index-2.html"><img src="images/logo.png" alt="" title=""></a></div>
+          <div class="menu-outer">
+            <!--Here Menu Will Come Automatically Via Javascript / Same Menu as in Header-->
+          </div>
+        </nav>
+      </div>
+      <!-- End Mobile Menu -->
+
+    </header>
+    <!-- End Main Header -->
+
+    <!-- Page Title -->
+    <section class="page-title_two" style="background-image:url(./images/T-4.jpg)">
+      <div class="page-title_two-gradient" style="background-image:url(images/background/pattern-10.png)"></div>
+      <div class="auto-container">
+        <h2>Our Projects</h2>
+        <ul class="clearfix bread-crumb">
+          <li><a href="index.html">Home</a></li>
+          <li>Projects</li>
+        </ul>
+      </div>
+    </section>
+    <!-- End Page Title -->
+
+    <section class="projects-one">
+      <div class="auto-container">
+
+        <div class="pt-5 pb-5 btn-group btn-group-justified filter-button-group" role="group" aria-label="filterImages">
+          <div class="btn-group button-border" role="group">
+            <button type="button" class="btn btn-default is-checked" data-filter=".MOTROLLA">MOTROLLA</button>
+          </div>
+          <div class="btn-group button-border" role="group">
+            <button type="button" class="btn btn-default" data-filter=".ERICSSON">ERICSSON</button>
+          </div>
+          <div class="btn-group button-border" role="group">
+            <button type="button" class="btn btn-default" data-filter=".OMANTEL">OMANTEL</button>
+          </div>
+          <div class="btn-group button-border" role="group">
+            <button type="button" class="btn btn-default is-checked" data-filter="OTC">OTC</button>
+          </div>
+          <div class="btn-group button-border" role="group">
+            <button type="button" class="btn btn-default" data-filter=".PDO">PDO</button>
+          </div>
+          <div class="btn-group button-border" role="group">
+            <button type="button" class="btn btn-default" data-filter=".OOREDOO">OOREDOO</button>
+          </div>
+          <div class="btn-group button-border" role="group">
+            <button type="button" class="btn btn-default is-checked" data-filter=".HUAWEI">HUAWEI</button>
+          </div>
+          <div class="btn-group button-border" role="group">
+            <button type="button" class="btn btn-default is-checked" data-filter=".NSN">NSN</button>
+          </div>
+          <div class="btn-group button-border" role="group">
+            <button type="button" class="btn btn-default is-checked" data-filter=".SIEMENS">SIEMENS</button>
+          </div>
+          <div class="btn-group button-border" role="group">
+            <button type="button" class="btn btn-default is-checked" data-filter=".ROP / RAFO">ROP /
+              RAFO</button>
+          </div>
+          <div class="btn-group button-border" role="group">
+            <button type="button" class="btn btn-default is-checked" data-filter=".FIBER OPTICS">FIBER
+              OPTICS</button>
+          </div>
+          <div class="btn-group button-border" role="group">
+            <button type="button" class="btn btn-default is-checked" data-filter=".ICT">
+              ICT</button>
+          </div>
+          <div class="btn-group button-border" role="group">
+            <button type="button" class="btn btn-default is-checked" data-filter=".HELIOS">HELIOS</button>
+          </div>
+          <div class="btn-group button-border" role="group">
+            <button type="button" class="btn btn-default is-checked" data-filter=".MEP">MEP</button>
+          </div>
+          <div class="btn-group button-border" role="group">
+            <button type="button" class="btn btn-default is-checked" data-filter=".VODAFONE">VODAFONE</button>
+          </div>
+
+        </div>
+        <!--filtering-->
+        <div class="grid">
+          <div class="grid-sizer">
+            <?php   foreach($projects as $project){
+// print_r($project);
+              ?>
+            <div class="col-xs-12 col-sm-6 col-md-4 grid-item MOTROLLA">
+              <div class="gallery-block_one-inner">
+                <div class="gallery-block_one-overlay">
+                </div>
+                <div style="display: flex;">
+                  <img src="./images/resource/Motrola.png" alt="Avatar" style="width:25%">
+                  <div>
+                    <h5 class="gallery-block_one-heading"><b><?php echo $project['title']; ?></b></h5>
+                    <h5 class="gallery-block_one-heading"><?php echo $project['period']; ?></h5>
+                  </div>
+                </div>
+                <?php echo $project['description']; ?>
+                
+              </div>
+
+            </div>
+
+            <?php } ?>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <!-- partial -->
+    <script src='https://code.jquery.com/jquery-2.2.4.min.js'></script>
+    <script src='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js'></script>
+    <script src='https://npmcdn.com/isotope-layout@3.0/dist/isotope.pkgd.min.js'></script>
+    <script src='https://cdn.jsdelivr.net/prettyphoto/3.1.6/js/jquery.prettyPhoto.js'></script>
+    <script src="./js/Project.js"></script>
+
+    <!-- Main Footer / Style Three -->
+    <footer class="main-footer style-three" style="background-color: #4c4c4c;">
+      <div class="auto-container">
+          <div class="inner-container">
+              <!-- Widgets Section -->
+              <div class="widgets-section">
+                  <div class="clearfix row">
+
+                      <!-- Big Column -->
+                      <div class="big-column col-lg-12 col-md-12 col-sm-12">
+                          <div class="clearfix row justify-content-center">
+
+                              <!-- Footer Column -->
+                              <div class="mx-auto footer_column col-lg-4 col-md-6 col-sm-12">
+                                  <div class="footer-widget contact-widget">
+                                      <h5 class="footer-title">VYK Network LLC</h5>
+                                      <ul class="footer-contact_list">
+                                          <li><span class="icon fas fa-map-marker-alt fa-fw"></span>
+                                              AHCL Ansal Town,<br>Indore,
+                                              India.</li>
+                                          <li><span class="icon fas fa-envelope fa-fw"></span>info@vyknetwork.com
+                                          </li>
+                                          <li><span class="icon fas fa-phone fa-fw"></span>+91 9131755703</li>
+                                      </ul>
+                                  </div>
+                              </div>
+
+                              <!-- Footer Column -->
+                              <div class="mx-auto footer_column col-lg-4 col-md-6 col-sm-12">
+                                  <div class="footer-widget contact-widget">
+                                      <h5 class="footer-title">WE Netwok LLC</h5>
+                                      <ul class="footer-contact_list">
+                                          <li><span class="icon fas fa-map-marker-alt fa-fw"></span>First floor,
+                                              0401-Z-319-KOM-4,<br>
+                                              Knowledge Oasis Muscat,<br>PO BOX: 1641, Postal Code: 114,<br>Al
+                                              Rusayl, Muscat, Sultanate of Oman</li>
+                                          <li><span
+                                                  class="icon fas fa-envelope fa-fw"></span>info@wenetworkllc.com
+                                          </li>
+                                          <li><span class="icon fas fa-phone fa-fw"></span>+968 24170030</li>
+                                      </ul>
+
+                                  </div>
+                              </div>
+
+                              <!-- Footer Column -->
+                              <div class="mx-auto footer_column col-lg-4 col-md-6 col-sm-12">
+                                  <div class="m-auto footer-widget links-widget">
+                                      <h5 class="footer-title">Links</h5>
+                                      <ul class="footer-list">
+                                          <li><a href="index.html">Home</a></li>
+                                          <li><a href="about.html">About Us</a></li>
+                                          <li><a href="services.html">Services</a></li>
+                                          <li><a href="product.html">Products</a></li>
+                                          <li><a href="projects.html">Projects</a></li>
+                                          <li><a href="gallery.html">Gallery</a></li>
+                                          <li><a href="contact.html">Contact Us</a></li>
+
+                                      </ul>
+                                  </div>
+                              </div>
+
+                          </div>
+                      </div>
+                  </div>
+              </div>
+          </div>
+      </div>
+      <div class="footer-bottom">
+          <div class="auto-container">
+              <div class="main-footer_copyright">Designed by <a href="#">Izh-tech</a></div>
+          </div>
+      </div>
+  </footer>
+    <!-- End Footer Style -->
+
+
+    <!-- End PageWrapper -->
+
+    <!-- Scroll To Top -->
+    <div class="scroll-to-top scroll-to-target" data-target="html"><span class="fas fa-arrow-up fa-fw"></span></div>
+
+    <script src="js/jquery.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/magnific-popup.min.js"></script>
+    <script src="js/appear.js"></script>
+    <script src="js/parallax.min.js"></script>
+    <script src="js/tilt.jquery.min.js"></script>
+    <script src="js/swiper.min.js"></script>
+    <script src="js/wow.js"></script>
+    <script src="js/parallax-scroll.js"></script>
+    <script src="js/gsap.min.js"></script>
+    <script src="js/SplitText.min.js"></script>
+    <script src="js/ScrollTrigger.min.js"></script>
+    <script src="js/ScrollToPlugin.min.js"></script>
+    <script src="js/ScrollSmoother.min.js"></script>
+
+    <script src="js/slick.js"></script>
+    <script src="js/slick-script.js"></script>
+    <script src="js/odometer.js"></script>
+    <script src="js/parallax-scroll.js"></script>
+    <script src="js/nav-tool.js"></script>
+    <script src="js/pagenav.js"></script>
+    <script src="js/jquery-ui.js"></script>
+    <script src="js/color-settings.js"></script>
+    <script src="js/script.js"></script>
+
+    <!--[if lt IE 9]><script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script><![endif]-->
+    <!--[if lt IE 9]><script src="js/respond.js"></script><![endif]-->
+
+</body>
+
+
+
+</html>
